289fa5431b93ca5b29e61f50d970d276dc4e95b5
Paul Day <paul.day.m@gmail.com>
2017-11-13 22:08:55 -0600

    fixed readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Next/React/Express JWT Auth Example
-Here's an example of using JWT to authenticate between two servers.
+Here's an example of using JWT to authenticate between two servers. The frontend is using React and Next.js. Routing is handled by Next Routes. State is handled by Redux and the Next Redux Wrapper. The backend is using Express - and is providing a mock user DB via a JSON file.
 
 To Install (with yarn, or use NPM):
 ```yarn install```
@@ -7,4 +7,6 @@ To Install (with yarn, or use NPM):
 To Run Both Servers:
 ```yarn run dev```
 
-then navigate to http://localhost:4000 in your browser!
+Note that on Windows, you may need to run the API seperately as its a combined bash command.
+
+Then navigate to http://localhost:4400 in your browser!
--- api_server/server.js
+++ api_server/server.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
-const port = 4500
-const cors = require('cors')
-const app = express()
-const cookieParser = require('cookie-parser')
-const masterRoutes = require('./routes/masterRoutes');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const port = 4500;
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+const masterRoutes = require("./routes/masterRoutes");
 
 const corsOptions = {
-    origin: ['http://localhost:4400'],
-    default: 'http://localhost:4400',
-    credentials: true
-  }
-  
-app.use(cors(corsOptions));
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+  origin: ["http://localhost:4400"],
+  default: "http://localhost:4400",
+  credentials: true
+};
 
-app.use(cookieParser())
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 masterRoutes(app);
 
-app.listen(port, "0.0.0.0", () => console.log(`API is listening on port ${port}`))
+app.listen(port, "0.0.0.0", () =>
+  console.log(`API is listening on port ${port}`)
+);
